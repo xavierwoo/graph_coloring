c7caf50078318b99a3f06d3a0cf1ede3de873a9c
xavierwoo <xavier.woo@gmail.com>
2025-03-20 22:50:10 +0800

    change file name

--- main.cpp
+++ main.cpp
@@ -5,7 +5,7 @@
 
 using fmt::print;
 int main() {
-    auto [v_num, edges] { graph_coloring::read_instance_file("../instances/DSJC500.9.col.txt")};
+    auto [v_num, edges] { graph_coloring::read_instance_file("../instances/DSJC500.5.col.txt")};
 
     if (v_num < 255) {
         graph_coloring::Solver<uint8_t> solver(std::move(edges));
